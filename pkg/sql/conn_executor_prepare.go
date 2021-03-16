9df39c74f08fd6966792615ef0beea22ca8c8cdb
Jennifer Lam <jl87@princeton.edu>
2021-03-16 18:17:07 -0400

    no hotkeys

--- pkg/sql/conn_executor_prepare.go
+++ pkg/sql/conn_executor_prepare.go
@@ -432,7 +432,7 @@ func isHotkey(key []byte) bool {
 	//hotkeys := []uint64{0}
 
 	keyInt := binary.BigEndian.Uint64(key)
-    if keyInt < 500000 {
+    if keyInt < 0 {
         return true
     }
 	//for _, hotkey := range hotkeys {
