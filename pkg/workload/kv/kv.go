fd115a5e5cc607ff49bcfabc42e3cf311faeeb9b
Jennifer Lam <jl87@princeton.edu>
2021-03-19 16:31:41 -0400

    Change connection pool

--- pkg/workload/kv/kv.go
+++ pkg/workload/kv/kv.go
@@ -263,7 +263,7 @@ func (w *kv) Ops(
 		return workload.QueryLoad{}, err
 	}
 	cfg := workload.MultiConnPoolCfg{
-		MaxTotalConnections: 200 * w.connFlags.Concurrency, //w.connFlags.Concurrency + 1,
+		MaxTotalConnections: 200, //w.connFlags.Concurrency + 1,
 	}
 	mcp, err := workload.NewMultiConnPool(cfg, urls...)
 	if err != nil {
