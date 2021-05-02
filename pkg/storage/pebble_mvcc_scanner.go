280ee29a71c308cf61b561e37f2faf114376d8b4
Jennifer Lam <jl87@princeton.edu>
2021-05-02 18:39:41 -0400

    debug

--- pkg/storage/pebble_mvcc_scanner.go
+++ pkg/storage/pebble_mvcc_scanner.go
@@ -12,7 +12,9 @@ package storage
 
 import (
 	"bytes"
+	"context"
 	"encoding/binary"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 	"sort"
 	"sync"
 
@@ -149,8 +151,11 @@ func (p *pebbleMVCCScanner) init(txn *roachpb.Transaction) {
 		p.txnEpoch = txn.Epoch
 		p.txnSequence = txn.Sequence
 		p.txnIgnoredSeqNums = txn.IgnoredSeqNums
-		//p.checkUncertainty = p.ts.Less(txn.MaxTimestamp)
-		p.checkUncertainty = false
+		p.checkUncertainty = p.ts.Less(txn.MaxTimestamp)
+		//p.checkUncertainty = false
+		if p.checkUncertainty {
+			log.Warningf(context.Background(), "jenndebug checkUncertainty txn %+v\n", txn)
+		}
 	}
 }
 
