727a17c7023fef2c5a32fa0646ab7ed53a134903
Jennifer Lam <jl87@princeton.edu>
2021-05-07 13:41:57 -0400

    debug read performance

--- pkg/sql/conn_executor_prepare.go
+++ pkg/sql/conn_executor_prepare.go
@@ -472,7 +472,7 @@ func isHotkey(key []byte) bool {
 	//hotkeys := []uint64{0}
 
 	keyInt := binary.BigEndian.Uint64(key)
-	if keyInt < 2 {
+	if keyInt < 250 {
 		return true
 	}
 	//for _, hotkey := range hotkeys {
