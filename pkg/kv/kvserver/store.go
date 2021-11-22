43ca9640087f70eb0ab182d00ddbd52cf32ed104
Jennifer Lam <jl87@princeton.edu>
2021-11-22 17:47:26 -0500

    wait time to 75

--- pkg/kv/kvserver/store.go
+++ pkg/kv/kvserver/store.go
@@ -2091,7 +2091,7 @@ func (s *Store) triggerRebalanceHotkeysAtInterval(ctx context.Context) {
 	// Wait until the workload is **probably** started. This is pretty hacky, but
 	// it'll probably get me correct results, and I couldn't care any less after that
 	// jenndebug
-	time.Sleep(150 * time.Second)
+	time.Sleep(75 * time.Second)
 
 	log.Warningf(ctx, "jenndebug promotion\n")
 
