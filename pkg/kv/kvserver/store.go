a1ff8070ae45c21445da197c2c1c8344dee733ce
Jennifer Lam <jl87@princeton.edu>
2021-12-03 15:08:26 -0500

    debug

--- pkg/kv/kvserver/store.go
+++ pkg/kv/kvserver/store.go
@@ -2245,7 +2245,7 @@ func (s *Store) triggerRebalanceHotkeysAtInterval(ctx context.Context) {
 	//TODO jenndebug make this an option somehow, or make the function a closure
 	interval := 10 * time.Second
 	promotionBatch := 5000
-	initialPromotionBatch := 5
+	initialPromotionBatch := 100
 
 	// connect to all CRDB servers
 	//port := 50055
