e5453cd31d2a76261ce195078288e81d4728b1d5
Jennifer Lam <jl87@princeton.edu>
2021-12-13 22:34:58 -0500

    debug

--- pkg/workload/kv/kv.go
+++ pkg/workload/kv/kv.go
@@ -383,7 +383,7 @@ func correctTxnParams(batchSize int, generateKey generateKeyFunc, greatestHotKey
 	argsInt := make([]int64, batchSize)
 	duplicates := make(map[int64]bool)
 	for i := 0; i < batchSize; i++ {
-		key := generateKey() + 1000000
+		key := generateKey()
 		for duplicates[key] {
 			key = generateKey()
 		}
