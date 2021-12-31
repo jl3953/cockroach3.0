e472e7ca329f1073c70b1317c5972623d5c53382
Jennifer Lam <jl87@princeton.edu>
2021-12-30 23:16:10 -0500

    debug

--- pkg/kv/kvserver/store.go
+++ pkg/kv/kvserver/store.go
@@ -2131,6 +2131,7 @@ func (s *Store) submitBatchToCicada(ctx context.Context,
 	defer cicadaCancel()
 
 	// send request to Cicada
+	log.Warningf(ctx, "jenndebug we made it\n")
 	batchSendTxnResp, sendErr := c.BatchSendTxns(cicadaCtx,
 		&smdbrpc.BatchSendTxnsReq{Txns: txnReqs})
 
