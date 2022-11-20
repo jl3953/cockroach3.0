3c79e0e6791de40ab921839b1646d586fbff689d
Jennifer Lam <jl87@princeton.edu>
2022-11-20 00:38:40 -0500

    batch size 20

--- pkg/kv/kvserver/store.go
+++ pkg/kv/kvserver/store.go
@@ -2173,7 +2173,7 @@ func (s *Store) submitBatchToCicada(ctx context.Context,
 func (s *Store) batchTxnsToCicada(ctx context.Context) {
 
 	batchingInterval := 10 * time.Millisecond
-	batchSize := 5
+	batchSize := 20
 	log.Warningf(ctx, "jenndebug Cicada batchingInterval %+v, batchSize %+v\n",
 		batchingInterval, batchSize)
 
