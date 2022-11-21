3702dece680f7804575ddc5dc20cf7115227a909
Jennifer Lam <jl87@princeton.edu>
2022-11-20 19:02:45 -0500

    timeout increase

--- pkg/kv/kvserver/store.go
+++ pkg/kv/kvserver/store.go
@@ -2172,7 +2172,7 @@ func (s *Store) submitBatchToCicada(ctx context.Context,
 
 func (s *Store) batchTxnsToCicada(ctx context.Context) {
 
-	batchingInterval := 5 * time.Millisecond
+	batchingInterval := 10 * time.Millisecond
 	batchSize := 1
 	log.Warningf(ctx, "jenndebug Cicada batchingInterval %+v, batchSize %+v\n",
 		batchingInterval, batchSize)
